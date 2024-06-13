1c34733055191bebafd978a70a3cf00d9498513f
glenmiracle18 <m.bonyu@alustudent.com>
2024-06-13 11:44:59 +0200

    fix: contributors summary copy

--- app/data/contributors.ts
+++ app/data/contributors.ts
@@ -1,8 +1,8 @@
 import { ContributorsDetailsProps } from "@/@types";
 
 export const ContributorsSummaryData: ContributorsDetailsProps[] = [
-  { title: "Full-Time Employee", value: "1.382", color: "blue" },
-  { title: "Part-Time Employee", value: "194", color: "green" },
-  { title: "Contract Employee", value: "283", color: "red" },
-  { title: "Internship Employee", value: "35", color: "orange" },
+  { title: "Male Contributors", value: "1.382", color: "blue" },
+  { title: "Female Contributors", value: "194", color: "green" },
+  { title: "Average Contributors", value: "283", color: "red" },
+  { title: "Average Dependents", value: "35", color: "orange" },
 ];
