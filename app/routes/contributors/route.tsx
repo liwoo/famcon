7b3876f05833bd41d2a37ef252ad621e921ae7fd
Glen Miracle <glen@Glens-MacBook-Pro.local>
2024-07-04 19:29:06 +0200

    update: dataTable

--- app/routes/contributors/route.tsx
+++ app/routes/contributors/route.tsx
@@ -1,14 +1,49 @@
 import MainLayout from "@/components/layouts/MainLayout";
+import * as React from "react";
+import {
+  type ColumnDef,
+  type ColumnFiltersState,
+  type SortingState,
+  type VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  ArrowUpDown,
+  ChevronDown,
+  CircleDollarSignIcon,
+  MoreHorizontal,
+  Plus,
+} from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Card } from "@/components/ui/card";
 import { DemographicMap } from "@/components/atoms/role-demographics/demographic-map";
 import { ContributorSummaryCard } from "@/components/atoms/contributors/contributor-summary-card";
-
-import { Card } from "@/components/ui/card";
 import { columns } from "@/components/contributions/columns";
-import { Plus } from "lucide-react";
-
 import { DataTable } from "@/components/contributions/data-table";
 import { contributors } from "@/components/contributions/data";
 
@@ -28,8 +63,7 @@ export default function Contributors() {
         </div>
         <Card className="p-4">
           {/* table goes here */}
-
-          {/* <DataTableSkeleton columnCount={10} /> */}
+          {/* <TableRender searchParams={searchParams} /> */}
           <DataTable columns={columns} data={contributors} />
         </Card>
       </div>
