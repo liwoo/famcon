491ded723381ea8891acfcf180cfe7267b1b6608
glenmiracle18 <m.bonyu@alustudent.com>
2024-10-18 14:44:30 +0200

    generate: new brach for recent firx 18/10/24

--- app/routes/contributors/route.tsx
+++ app/routes/contributors/route.tsx
@@ -1,51 +1,47 @@
-import MainLayout from '@/components/layouts/MainLayout';
+import MainLayout from "@/components/layouts/MainLayout";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
-import { ContributorSummaryCard } from '@/components/atoms/contributors/contributor-summary-card';
+import { ContributorSummaryCard } from "@/components/atoms/contributors/contributor-summary-card";
 
-import { Card } from '@/components/ui/card';
-import { Plus } from 'lucide-react';
+import { Card } from "@/components/ui/card";
+import { Plus } from "lucide-react";
 
-import { columns } from './components/columns';
-import { contributors } from '@/data/contributors/data';
-import { CrudList } from '@/components/blocks/crud-list';
-import { TContributor, TPartialContributor } from '@/@types/contributors';
-import { ContributorService } from '@/services/contributors/contributor-service';
-import { useNavigate } from '@remix-run/react';
+import { columns } from "./components/columns";
+import { contributors } from "@/data/contributors/data";
+import { CrudList } from "@/components/blocks/crud-list";
+import { TContributor, TPartialContributor } from "@/@types/contributors";
+import { ContributorService } from "@/services/contributors/contributor-service";
+import { useNavigate } from "@remix-run/react";
 
 // @Authorized(['admin'])
 export default function Contributors() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <MainLayout
-            title="Contributors"
-            action={
-                <Button
-                    className="bg-teal-900"
-                    onClick={() => navigate('/contributors/create')}
-                >
-                    <Plus /> Add Contributor
-                </Button>
-            }
+  return (
+    <MainLayout
+      title="Contributors"
+      action={
+        <Button
+          className="bg-teal-900 dark:text-white text-white"
+          onClick={() => navigate("/contributors/create")}
         >
-            <div className="flex flex-col gap-4">
-                <div className="flex gap-2 w-full">
-                    <ContributorSummaryCard />
-                </div>
-                <Card className="p-4">
-                    <CrudList<
-                        TPartialContributor,
-                        TContributor,
-                        ContributorService
-                    >
-                        columns={columns}
-                        data={contributors}
-                        baseRoute={'/contributors'}
-                    ></CrudList>
-                </Card>
-            </div>
-        </MainLayout>
-    );
+          <Plus /> Add Contributor
+        </Button>
+      }
+    >
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-2 w-full">
+          <ContributorSummaryCard />
+        </div>
+        <Card className="p-4">
+          <CrudList<TPartialContributor, TContributor, ContributorService>
+            columns={columns}
+            data={contributors}
+            baseRoute={"/contributors"}
+          ></CrudList>
+        </Card>
+      </div>
+    </MainLayout>
+  );
 }
